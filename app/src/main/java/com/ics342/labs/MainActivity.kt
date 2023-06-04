4fba6a69eff25040d74c01de5df540c5e4aef030
6urk <82283209+6urk@users.noreply.github.com>
2023-06-04 12:25:43 -0500

    finished lab 2

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,38 +1,50 @@
 package com.ics342.labs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.editText)
+
+        button.setOnClickListener {
+            handleButtonClick()
+        }
     }
 
     private fun handleButtonClick() {
-        /** Implement the functionality to display the alert here. **/
+        val text = editText.text.toString()
+        if (text.isNotEmpty()) {
+            showTextInAlert(text)
+        } else {
+            showErrorAlert()
+        }
     }
 
     private fun showTextInAlert(text: String) {
-        AlertDialog
-            .Builder(this)
-            .setTitle(R.string.entered_text)
+        AlertDialog.Builder(this)
+            .setTitle("Entered text")
             .setMessage(text)
-            .setPositiveButton(R.string.okay, null)
+            .setPositiveButton("Okay", null)
             .create()
             .show()
     }
 
     private fun showErrorAlert() {
-        AlertDialog
-            .Builder(this)
-            .setTitle(R.string.error_title)
-            .setMessage(R.string.error_message)
-            .setPositiveButton(R.string.okay, null)
+        AlertDialog.Builder(this)
+            .setTitle("Error")
+            .setMessage("Enter some text in the text field.")
+            .setPositiveButton("Okay", null)
             .create()
             .show()
     }
