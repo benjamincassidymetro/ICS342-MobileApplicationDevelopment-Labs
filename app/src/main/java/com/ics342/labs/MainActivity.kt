82bb262111f948a1fe412c32d1910e7276eabfd6
JoshStout <JoshStout>
2023-06-11 14:05:23 -0500

    Lab 3

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,13 +3,26 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 
@@ -42,8 +55,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("Android")
+                    DataItemList(dataItems = dataItems)
                 }
             }
         }
@@ -61,11 +78,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DataItemView(dataItem: DataItem) {
     /* Create the view for the data item her. */
+
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier.height(56.dp).fillMaxWidth(),
+    ){
+        Text(
+            text = "${dataItem.id}",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.width(25.dp))
+        Text(
+            text = dataItem.name,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+    Text(
+        text = dataItem.description,
+        fontSize = 20.sp,
+        textAlign = TextAlign.Left,
+        modifier = Modifier.fillMaxWidth()
+    )
+    Text(
+        text = "",
+        fontSize = 20.sp
+    )
+
 }
 
 @Composable
 fun DataItemList(dataItems: List<DataItem>) {
     /* Create the list here. This function will call DataItemView() */
+
+    LazyColumn(){
+        items(items = dataItems){
+            DataItemView(dataItem = it)}
+        }
 }
 
 @Preview(showBackground = true)
