b7a5f78e7d7a4668bdde891fa63c6c6e36b2267f
Bee-Xiong <bxiong1990@gmail.com>
2023-06-26 10:28:09 -0500

    Assignment 1

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,134 +3,150 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ics342.labs.data.DataItem
-import com.ics342.labs.ui.theme.LabsTheme
+import androidx.compose.ui.unit.sp
 
-private val dataItems = listOf(
-    DataItem(1, "Item 1", "Description 1"),
-    DataItem(2, "Item 2", "Description 2"),
-    DataItem(3, "Item 3", "Description 3"),
-    DataItem(4, "Item 4", "Description 4"),
-    DataItem(5, "Item 5", "Description 5"),
-    DataItem(6, "Item 6", "Description 6"),
-    DataItem(7, "Item 7", "Description 7"),
-    DataItem(8, "Item 8", "Description 8"),
-    DataItem(9, "Item 9", "Description 9"),
-    DataItem(10, "Item 10", "Description 10"),
-    DataItem(11, "Item 11", "Description 11"),
-    DataItem(12, "Item 12", "Description 12"),
-    DataItem(13, "Item 13", "Description 13"),
-    DataItem(14, "Item 14", "Description 14"),
-    DataItem(15, "Item 15", "Description 15"),
-    DataItem(16, "Item 16", "Description 16"),
-    DataItem(17, "Item 17", "Description 17"),
-    DataItem(18, "Item 18", "Description 18"),
-    DataItem(19, "Item 19", "Description 19"),
-    DataItem(20, "Item 20", "Description 20"),
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    //Greeting("Android")
-                    DataItemList(dataItems)
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                MyApp()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyApp() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TitleBar()
+        Location()
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CurrentTemp()
+            }
+            Spacer(modifier = Modifier.width(width = 75.dp))
+            MyImage()
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp)
+        ) {
+            MoreInfo()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TitleBar() {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(R.string.app_name))
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Gray)
     )
 }
 
 @Composable
-fun DataItemView(dataItem: DataItem) {
-    Row{
-        Text(text = "${dataItem.id}",
-            style = MaterialTheme.typography.headlineLarge,)
-        Spacer(modifier = Modifier.size(4.dp))
-        Column{
-            Text(text = "${dataItem.name}",
-                style = MaterialTheme.typography.headlineLarge,)
-            Spacer(modifier = Modifier.size(4.dp))
-            Text(text = "${dataItem.description}",
-                style = MaterialTheme.typography.bodySmall,)
-            
-        }
-        
-    }
+fun Location() {
+    Text(
+        text = stringResource(R.string.location),
+        style = TextStyle(
+            fontSize = 20.sp,
+        )
+    )
 }
-
 
 @Composable
-fun DataItemList(dataItems: List<DataItem>) {
-    LazyColumn{
-        items(dataItems.size){item->
-            var showDialog by remember { mutableStateOf(false) }
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .clickable(onClick = {showDialog = true})
-            ){
-                DataItemView(dataItems[item])
-            }
-
-            if (showDialog) {
-                AlertDialog(
-                    onDismissRequest = { showDialog = false },
-                    title = { Text(text = "${dataItems[item].name}") },
-                    text = { Text("${dataItems[item].description}") },
-                    confirmButton = {
-                        Button(
-                            onClick = {
-                                showDialog = false
-                            }) {
-                            Text("Okay")
-                        }
-                    },
-                )
-            }
-        }
-    }
+fun CurrentTemp() {
+    Text(
+        text = stringResource(R.string.currTemp),
+        style = TextStyle(
+            fontSize = 75.sp,
+        )
+    )
+    Text(
+        text = stringResource(R.string.feel),
+    )
 }
 
-@Preview
 @Composable
-fun GreetingPreview() {
-    LabsTheme {
-        Greeting("Android")
-    }
+fun MyImage() {
+    Image(
+        painter = painterResource(id = R.drawable._1d),
+        contentDescription = stringResource(R.string.image_description),
+        modifier = Modifier.size(100.dp)
+    )
 }
+
+@Composable
+fun MoreInfo() {
+    Text(
+        text = stringResource(R.string.low),
+        style = TextStyle(
+            fontSize = 20.sp,
+        )
+    )
+    Text(
+        text = stringResource(R.string.high),
+        style = TextStyle(
+            fontSize = 20.sp,
+        )
+    )
+    Text(
+        text = stringResource(R.string.humidity),
+        style = TextStyle(
+            fontSize = 20.sp,
+        )
+    )
+    Text(
+        text = stringResource(R.string.pressure),
+        style = TextStyle(
+            fontSize = 20.sp,
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp()
+}
+
