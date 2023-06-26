90237f9516ca2ce51464cf18a23812380d9ece64
Matthew Davis <marrthistyping64@gmail.com>
2023-06-25 23:23:51 -0500

    worked on assignment1 more

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,34 +1,26 @@
 package com.ics342.labs
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text("Hello World")
+                    DataItemList()
                 }
             }
         }
@@ -49,18 +41,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DataItemList() {
-    Column{
-        Text("My Weather App")
-        Text("Saint Paul, MN")
-        Text("72")
-        Text("Feels like 78")
-        // image()
-        Text("Low 65")
-        Text("High 80")
-        Text("Humidity 100%")
-        Text("Pressure 1023 hPa")
+    MaterialTheme(
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(
+                        text = stringResource(id = R.string.title),
+                        color = Color.DarkGray)},
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = Color.LightGray
+                    )
+                )
+            },
+
+        ) {
+            Column {
+                Text("Hi")
+                Text("Hi")
+                Text("Hi")
+                Text(text = stringResource(id = R.string.location))
+                Text(text = stringResource(id = R.string.temp))
+                Text(text = stringResource(id = R.string.temp_feel))
+                Text(text = stringResource(id = R.string.low_temp))
+                Text(text = stringResource(id = R.string.high_temp))
+                Text(text = stringResource(id = R.string.humidity))
+                Text(text = stringResource(id = R.string.pressure))
+            }
+
+
+         }
+        }
     }
-}
+
 
 @Preview(showBackground = true)
 @Composable
