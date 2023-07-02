4b3dc639955c0dda4e60f556fe8973e4a1b74878
Matthew Davis <marrthistyping64@gmail.com>
2023-07-01 19:50:31 -0500

    fixed androidstudio bug

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.ics342.labs
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
@@ -11,21 +10,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 
@@ -62,41 +59,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DataListScreen(dataItems)
+                    NavExample()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun DataListScreen(items: List<DataItem>){
-    var showDialog by remember { mutableStateOf(false) }
-    var dataItem by remember { mutableStateOf<DataItem?>(null) }
-    // when selected, it focuses on the specific item and sets showDialog to true
-    DataItemList(items) { dataItem = it;
-                          showDialog=true }
-    dataItem?.let{
-        // if person clicks on item,
-        if (showDialog) {
-            AlertDialog(
-                // close if user clicks outside the box
-                onDismissRequest = { showDialog = false },
-                title = { Text("${it.name}") },
-                text = { Text("${it.description}") },
-                // close if user presses disagree button
-                dismissButton = {
-                    Button(onClick = { showDialog = false }) {
-                        Text("Disagree")
-                    }
-                },
-                // closes if user presses agree button
-                confirmButton = {
-                    Button(onClick = { showDialog = false } ) {
-                        Text("Agree")
-                    }
-                },
-            )
+fun NavExample(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "DataItemMenu") {
+        this.composable("DataItemMenu") {
+            DataItemList(dataItems, navController)
+        }
+        composable("DataItemInfo") {
         }
     }
 }
@@ -121,15 +99,15 @@ fun DataItemView(dataItem: DataItem) {
         )
         Column{
             Text(text = dataItem.name,
-                 style = TextStyle(
+                style = TextStyle(
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold
-                 )
+                )
             )
             Text(text = dataItem.description,
-                 style = TextStyle(
+                style = TextStyle(
                     fontSize =10.sp,
-                 )
+                )
             )
         }
     }
@@ -146,29 +124,32 @@ dataItemView()
  */
 
 // this code enables each item to be clickable and puts the view in each column
+
 @Composable
-fun DataItemList(
-    dataItems: List<DataItem>,
-    dataItemClicked: (DataItem) -> Unit
-) {
-    LazyColumn{
-        items(dataItems){ dataItem ->
-           Box(
-              modifier = Modifier.clickable {
-                  dataItemClicked(dataItem)
-              }
-           ) {
-               DataItemView(dataItem)
-           }
+fun DataItemList(dataItems: List<DataItem>, navController : NavController) {
+    LazyColumn {
+        items(dataItems) { dataItem ->
+            Box(
+                modifier = Modifier.clickable {
+                    navController.navigate("DataItemInfo")
+                }
+            ) {
+                DataItemView(dataItem)
+            }
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LabsTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            DataListScreen(dataItems)
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            NavExample()
+            //DataItemList(dataItems)
         }
     }
 }
