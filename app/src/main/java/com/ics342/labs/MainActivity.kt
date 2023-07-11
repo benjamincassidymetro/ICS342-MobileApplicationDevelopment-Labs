104c13e4dd2b0ff91003a8c6c872f8501b0110a3
6urk <82283209+6urk@users.noreply.github.com>
2023-07-10 23:20:48 -0500

    commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,51 +1,161 @@
+
 package com.ics342.labs
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ics342.labs.ui.theme.LabsTheme
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var editText: EditText
-    private lateinit var button: Button
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            LabsTheme {
+                val navController = rememberNavController()
 
-        button = findViewById(R.id.button)
-        editText = findViewById(R.id.editText)
-
-        button.setOnClickListener {
-            handleButtonClick()
+                NavHost(navController = navController, startDestination = "weather") {
+                    composable("weather") {
+                        WeatherScreen(navController)
+                    }
+                    composable("forecast") {
+                        ForecastScreen()
+                    }
+                }
+            }
         }
     }
+}
 
-    private fun handleButtonClick() {
-        val text = editText.text.toString()
-        if (text.isNotEmpty()) {
-            showTextInAlert(text)
-        } else {
-            showErrorAlert()
+@Composable
+fun WeatherScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .padding(vertical = 16.dp)
+                .padding(start = 16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, color = Color.Black)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.city_name),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 18.sp, color = Color.Black),
+            modifier = Modifier.padding(bottom = 20.dp).align(Alignment.CenterHorizontally)
+
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = stringResource(R.string.temperature),
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 70.sp,
+                        color = Color.Black
+                    ),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = stringResource(R.string.feels_like),
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.Black
+                    ),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            }
+            Image(
+                painter = painterResource(R.drawable.sun_icon),
+                contentDescription = stringResource(R.string.weather_icon),
+                modifier = Modifier.size(120.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.low),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, color = Color.Black),
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .padding(start = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.high),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, color = Color.Black),
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .padding(start = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.humidity),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, color = Color.Black),
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .padding(start = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.pressure),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 20.sp, color = Color.Black),
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .padding(start = 16.dp)
+        )
+
+        Button(
+            onClick = { navController.navigate("forecast") },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Forecast")
         }
     }
+}
 
-    private fun showTextInAlert(text: String) {
-        AlertDialog.Builder(this)
-            .setTitle("Entered text")
-            .setMessage(text)
-            .setPositiveButton("Okay", null)
-            .create()
-            .show()
-    }
+@Composable
+@Preview
+fun MainActivityPreview() {
+    LabsTheme {
+        val navController = rememberNavController()
 
-    private fun showErrorAlert() {
-        AlertDialog.Builder(this)
-            .setTitle("Error")
-            .setMessage("Enter some text in the text field.")
-            .setPositiveButton("Okay", null)
-            .create()
-            .show()
+        NavHost(navController = navController, startDestination = "weather") {
+            composable("weather") {
+                WeatherScreen(navController)
+            }
+            composable("forecast") {
+                ForecastScreen()
+            }
+        }
     }
 }
