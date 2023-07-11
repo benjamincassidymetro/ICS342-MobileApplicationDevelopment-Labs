b10e961d2330a9c60451e2058003d69e75733469
Mauricio <fuentesmauricio75@gmail.com>
2023-07-10 21:20:28 -0500

    commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ics342.labs.ui.theme.LabsTheme
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +33,26 @@ class MainActivity : ComponentActivity() {
                     /*
                     Display the items from the Json file in a LazyColumn
                      */
+                    LazyColumn (){
+                        items(data){
+                            dataItem -> DataItemView(dataItem = dataItem)
+                            
+                        }
+
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun DataItemView(dataItem: DataItem) {
+        Text(text = dataItem.id.toString())
+        Text(text = dataItem.giveName)
+        Text(text = dataItem.familyName)
+        Text(text = dataItem.age.toString())
+
 }
 
 private fun loadData(resources: Resources): String {
@@ -40,8 +62,13 @@ private fun loadData(resources: Resources): String {
         .use { it.readText() }
 }
 
-private fun dataFromJsonString(json: String): List</* Add your data type here */> {
-    val moshi: Moshi = Moshi.Builder().build()
-    val jsonAdapter: JsonAdapter<List</* Put your data type here */>> = moshi.adapter<List</* put your data type here*/>>()
-    return jsonAdapter.fromJson(json)
+@OptIn(ExperimentalStdlibApi::class)
+private fun dataFromJsonString(json: String): List<DataItem> {
+    val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+    val listType = Types.newParameterizedType(List::class.java, DataItem::class.java)
+    val jsonAdapter: JsonAdapter<List<DataItem>> = moshi.adapter<List<DataItem>>(listType)
+    return jsonAdapter.fromJson(json) ?: listOf()
 }
+
+
+
